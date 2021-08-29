78a34718ddf9fb17af2e3cdcab5f8bcc2a61d916
Rui Vergani Neto <ruiverganineto@gmail.com>
2021-08-29 10:02:51 +0100

    section depoimentos responsive done

--- js/main.js
+++ js/main.js
@@ -7,6 +7,20 @@
           el: ".s-card-depoimentos .container .top .swiper-pagination",
           clickable: true,
         },
+        breakpoints:{
+          320 : {
+              slidesPerView: 1.3,
+              spaceBetween: 16,
+          },
+          600: {
+            slidesPerView: 2.3,
+            spaceBetween: 15,
+          },
+          1200: {
+              slidesPerView: 3,
+              spaceBetween: 32,
+          }
+        }
     });
 
 // Script to change background color 
